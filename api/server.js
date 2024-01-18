6461748cb4807d8933a1b1b2c5ecec68a7d34b25
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-18 20:36:21 +0545

    first commit

---
+++ api/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server started at port number :${port}`);
+});

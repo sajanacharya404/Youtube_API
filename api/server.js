da4e6e23f89c7562b801de538841dbf9a092ae6d
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-18 21:17:14 +0545

    middleware added for our app

--- api/server.js
+++ api/server.js
@@ -1,10 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import { connectDB } from "./db/db.js";
 
 connectDB();
 const app = express();
+
+//middleware
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+app.use(express.urlencoded());
 
 const port = process.env.PORT;
 app.listen(port, () => {

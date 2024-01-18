648e8971f161fd86b2b7fde73780374824ee9dec
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-18 21:27:28 +0545

    async handler created

---
+++ api/utils/asyncHandler.js
@@ -0,0 +1,10 @@
+export const asyncHandler = (fn) => async (req, res, next) => {
+  try {
+    await fn(req, res, next);
+  } catch (error) {
+    res.status(err.code || 500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+};

8fb8ca5ee58e79e63d409f3eaa3e3ebaa84412d0
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-18 22:59:50 +0545

    Cloudinary amd multer install and setup for image and video upload

---
+++ api/utils/cloudinary.js
@@ -0,0 +1,8 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});

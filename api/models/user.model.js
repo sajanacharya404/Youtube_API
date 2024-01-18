fe89951203de7487a9cb4c42855cfa0c056def18
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-18 22:24:20 +0545

    bcrypt used for password hashing and comparing in usermodel

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new Schema(
   {
@@ -46,5 +47,15 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export const User = mongoose.model("User", userSchema);

ed006c44030a7ae4c2f84afb12062800b08efb74
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-19 22:14:12 +0545

    register controller completed with ApiError and ApiResponse utils

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,6 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
@@ -12,33 +13,52 @@ const registerUser = asyncHandler(async (req, res) => {
   ) {
     throw new ApiError(400, "All fields are required");
   }
+
+  //check existing users
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+  });
+  if (existedUser) {
+    throw new ApiError(409, "User with email and username already exists");
+  }
+
+  //local path
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  //check avatar
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar field is required");
+  }
+
+  //upload on cloudinary
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  //check avatar for cloudinary
+  if (!avatar) {
+    throw new ApiError(400, "Avatar field is required");
+  }
+
+  //databse entry
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+  if (!createdUser) {
+    throw new ApiError(500, "Something went wrong while registering user");
+  }
+
+  //response
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User Registered SuccessFully"));
 });
-//check existing users
-const existedUser = User.findOne({
-  $or: [{ username }, { email }],
-});
-if (existedUser) {
-  throw new ApiError(409, "User with email and username already exists");
-}
-
-//local path
-const avatarLocalPath = req.files?.avatar[0]?.path;
-const coverImageLocalPath = req.files?.coverImage[0]?.path;
-
-//check avatar
-if (!avatarLocalPath) {
-  throw new ApiError(400, "Avatar field is required");
-}
-
-//upload on cloudinary
-const avatar = await uploadOnCloudinary(avatarLocalPath);
-const coverImage = await uploadOnCloudinary(coverImageLocalPath);
-
-//check avatar for cloudinary
-if (!avatar) {
-  throw new ApiError(400, "Avatar field is required");
-}
-
-//databse entry
-
 export { registerUser };

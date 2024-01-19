49e639dfe5896320f99b221e2326631b0536bff6
sikklelikestraight <sesmaniraj7@gmail.com>
2024-01-19 22:02:24 +0545

    User controller created for taking data field from req.body and upload image through multer and  then to cloudinary.

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,10 +1,44 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    message: "ok",
-  });
+  //get user from frontend
+  const { username, email, fullName, password } = req.body;
+  //check existing user
+  if (
+    [username, email, fullName, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
 });
+//check existing users
+const existedUser = User.findOne({
+  $or: [{ username }, { email }],
+});
+if (existedUser) {
+  throw new ApiError(409, "User with email and username already exists");
+}
+
+//local path
+const avatarLocalPath = req.files?.avatar[0]?.path;
+const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+//check avatar
+if (!avatarLocalPath) {
+  throw new ApiError(400, "Avatar field is required");
+}
+
+//upload on cloudinary
+const avatar = await uploadOnCloudinary(avatarLocalPath);
+const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+//check avatar for cloudinary
+if (!avatar) {
+  throw new ApiError(400, "Avatar field is required");
+}
+
+//databse entry
 
 export { registerUser };
--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
-router.post("/register", registerUser);
+router.post(
+  "/register",
+  upload.fields([
+    { name: "avtar", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  registerUser
+);
 export default router;
